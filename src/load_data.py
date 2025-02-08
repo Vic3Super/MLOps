a6cbfd5631d019dcacee323cce42b73639c74509
Vic3Super <vic3super@hotmail.de>
2025-02-08 21:15:54 +0100

    updates

--- src/load_data.py
+++ src/load_data.py
@@ -8,7 +8,7 @@ from feast import FeatureView, FeatureStore
 
 def load_data_from_feature_store(size = 1000):
 
-    store = FeatureStore(repo_path=".")
+    store = FeatureStore(repo_path="/app/src/feature_store.yaml")
     feature_service = store.get_feature_service("taxi_drive")
 
     data_table = store.get_data_source("trip_source").get_table_query_string()
