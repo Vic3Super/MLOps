80bf7f589f3aac7c9caf7b7789aa85a52780bc4a
Vic3Super <vic3super@hotmail.de>
2025-02-08 21:42:10 +0100

    updates

--- src/load_data.py
+++ src/load_data.py
@@ -1,14 +1,23 @@
+from feast.infra.offline_stores.bigquery import BigQueryOfflineStoreConfig
+from feast.repo_config import RegistryConfig
 from google.cloud import bigquery
 import pandas as pd
 import json
 import logging
-from feast import FeatureView, FeatureStore
-
+from feast import FeatureView, FeatureStore, RepoConfig
 
 
 def load_data_from_feature_store(size = 1000):
 
-    store = FeatureStore(repo_path="/app/src/feature_store.yaml")
+    repo_config = RepoConfig(
+        registry=RegistryConfig(path="gs://feast_registry/registry.db"),
+        project="feast_demo_aws",
+        provider="gcp",
+        offline_store=BigQueryOfflineStoreConfig(),
+
+    )
+
+    store = FeatureStore(repo_path="/app/src")
     feature_service = store.get_feature_service("taxi_drive")
 
     data_table = store.get_data_source("trip_source").get_table_query_string()
