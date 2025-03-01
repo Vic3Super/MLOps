ed579beaf6894696421a2e88b7c6598a04854ae2
Vic3Super <vic3super@hotmail.de>
2025-03-01 20:00:21 +0100

    updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 export FEATURE_STORE_PATH=/app/src/
 
-#export GOOGLE_APPLICATION_CREDENTIALS=/app/configs/keys.json
 
 
 DATASET_NAME=$(jq -r '.DATASET_NAME' /app/configs/config.json)
@@ -13,23 +12,7 @@ export TEST_RUN
 TRAINING_SIZE=$(jq -r '.TRAINING_SIZE' /app/configs/config.json)
 export TRAINING_SIZE
 
-gcloud config set project PROJECT_ID
 
 python /app/src/main.py
 
 
-#RUN_ID=$(jq -r '.RUN_ID' /app/configs/config.json)
-
-#export RUN_ID
-
-
-# Fetch the dynamically generated requirements.txt from Google Cloud Storage
-#gsutil cp "$RUN_ID/artifacts/model/requirements.txt" /app/requirements.txt
-
-#gsutil cp /app/logs/app.log "$RUN_ID/artifacts/artifacts/app.log"
-
-# Install dependencies from the dynamically generated requirements
-#pip install --no-cache-dir -r /app/requirements.txt
-
-# Start the application
-#python app.py
